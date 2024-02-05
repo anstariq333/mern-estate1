17f8b0ec66cde5fee3dd001a07a14f269ee307f2
Ans Tariq <anstariq333@gmail.com>
2024-02-05 08:07:56 +0500

    create Signup api and middleware ans function to handle possible error

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then( ()=>{
+mongoose.connect(process.env.MONGO)
+.then( ()=>{
      console.log('connected to mongodb');
 
 })
@@ -24,3 +25,14 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+  
+
+app.use( (err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+   return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+});
+});
