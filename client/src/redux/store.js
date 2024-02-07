b00fdb5a1ad5ed956dfa970825604eb4887896b4
Ans Tariq <anstariq333@gmail.com>
2024-02-07 22:55:39 +0500

    add redux presist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,24 @@
-import { configureStore, Tuple } from '@reduxjs/toolkit'
-import { useReducer } from './user/userSlice.js';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js'; // Assuming useReducer was a typo and it should be userReducer
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-configureStore({
-  reducer: {user: useReducer},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware ({
-    serializableCheck: false,
-}),
-})
+const rootReducer = combineReducers({ user: userReducer }); // Assuming useReducer was a typo and it should be userReducer
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer); // Changed the local variable name to persistedReducer
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
